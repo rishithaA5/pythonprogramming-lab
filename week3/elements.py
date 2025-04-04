808accaecb6bc99a4b1043ae17de0a9658dab916
rishithaA5 <nrishitha71@gmail.com>
2025-04-04 16:00:35 +0530

    Update and rename README.md to week3/elements.py

---
+++ week3/elements.py
@@ -0,0 +1,7 @@
+elements=[]
+limit=int(input("enter the elements"))
+for i in range(limit):
+    ele=int(input())
+    elements.append(ele)
+for i in range(len(elements)):
+    print(elements[i],end=' ')
